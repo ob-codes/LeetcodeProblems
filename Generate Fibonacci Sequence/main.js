d6c30bb94cf9e44fdad5d551e17c6a4daf59367b
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-18 18:56:49 +0500

    first approach with custom objects - LC Easy

--- Generate Fibonacci Sequence/main.js
+++ Generate Fibonacci Sequence/main.js
@@ -2,6 +2,21 @@
  * @return {Generator<number>}
  */
 var fibGenerator = function() {
+  function MakeSeries() {
+      let temp = 0;
+      this.prev = 0;
+      this.curr = 0;
+      this.value = 0;
+      this.next = function() {
+          this.value = this.prev + this.curr;
+          
+          this.value == 0 ? this.curr = 1 : 0;
+          this.prev = this.curr ;
+          this.curr = this.value;
+          return this;
+      }
+  }
+  return new MakeSeries();
 };
 
 
