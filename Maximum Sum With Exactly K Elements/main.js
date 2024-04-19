db0cc849846dd8e4a9b551e57ee20a6db11abe7a
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-19 23:25:28 +0500

    [Final] LC Easy

--- Maximum Sum With Exactly K Elements/main.js
+++ Maximum Sum With Exactly K Elements/main.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 var maximizeSum = function(nums, k) {
-  
+  let sum=0;
+  nums.sort((a, b) => a-b);
+  for (let i=0; i< k; i++) {
+      let temp = nums.pop();
+      sum += temp;
+      ++temp;
+      nums.push(temp);
+  }
+  return sum;
 };
 
 console.log(maximizeSum([1,2,3,4,5], 3));//18
