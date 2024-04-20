4ce858eb83d293e0fddb74df8c225257a7d68183
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-20 16:57:41 +0500

    LC Easy - mathematical logic

---
+++ Pass the Pillow/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} time
+ * @return {number}
+ */
+var passThePillow = function(n, time) {
+  
+};
+
+console.log(passThePillow(4, 5));//2
+console.log(passThePillow(3, 2));//3
+console.log(passThePillow(8, 9));//6
+console.log(passThePillow(6, 8));//3
+console.log(passThePillow(5, 3));//4
