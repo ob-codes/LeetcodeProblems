9fe525dd057e9e3861b9d8f74313cb06d01f1116
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-20 16:58:00 +0500

    LC Easy - added solution

--- Pass the Pillow/main.js
+++ Pass the Pillow/main.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 var passThePillow = function(n, time) {
+  if (n > time) 
+    return time + 1;
   
+  let noOfCircle = Math.floor(time / (n -1));
+  let position = time % (n - 1);
+  if (noOfCircle % 2 !== 0)
+    return n - position;
+  else if (noOfCircle % 2 == 0)
+    return position +1;
 };
 
 console.log(passThePillow(4, 5));//2
