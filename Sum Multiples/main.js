cfc700f4768cf3858c49b547f1b994c4e3976ff6
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-21 17:01:34 +0500

    [Final] LC Easy

--- Sum Multiples/main.js
+++ Sum Multiples/main.js
@@ -3,6 +3,11 @@
  * @return {number}
  */
 var sumOfMultiples = function(n) {
+  if (n < 1) return 0;
+  if (n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+      return n + sumOfMultiples(n -1);
+  else
+      return sumOfMultiples(n -1);
 };
 
 console.log(sumOfMultiples(7));//21
