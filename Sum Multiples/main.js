6ecb3945780d3157431c189fbd12b33a5984ab45
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-21 17:01:01 +0500

    LC Easy - recursion

---
+++ Sum Multiples/main.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function(n) {
+};
+
+console.log(sumOfMultiples(7));//21
+console.log(sumOfMultiples(10));//40
+console.log(sumOfMultiples(9));//30
