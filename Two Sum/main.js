fb82ce92c96f598521a7a7f1f8a0400b7044300d
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-22 23:04:54 +0500

    [Final] LC Easy - hashmap

--- Two Sum/main.js
+++ Two Sum/main.js
@@ -4,12 +4,27 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+  /*
   for (let i=0; i< nums.length; i++) {
     for (let j=i+1; j< nums.length; j++) {
       if (nums[i] + nums[j] == target)
         return [i, j];
     }
   }
+  */
+  let mpp = new Map();
+
+  for (let i=0; i < nums.length; i++) {
+    let diff = target - nums[i];
+
+    if (mpp.has(diff)) {
+      return [mpp.get(diff), i] 
+    }
+
+    mpp.set(nums[i], i);
+  }
+
+  return [0, 0];
 };
 
 console.log(twoSum([2,7,11,15], 9));//[0,1]
