681da2d29d7bf28ac9c54d6a7c713f127def7d91
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-23 21:35:55 +0500

    [Final] LC Easy - switch & if

--- Roman to Integer/main.js
+++ Roman to Integer/main.js
@@ -54,3 +54,5 @@ var romanToInt = function(s) {
 
 console.log(romanToInt("III"));//3
 console.log(romanToInt("LVIII"));//58
+console.log(romanToInt("MCMXCIV"));//1994
+console.log(romanToInt("XLIX"));//49
