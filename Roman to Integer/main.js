9f54a87f4517ddeb0c04b5f42d35f91257885aba
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-23 21:35:36 +0500

    LC Easy - added logic

--- Roman to Integer/main.js
+++ Roman to Integer/main.js
@@ -7,7 +7,47 @@ var romanToInt = function(s) {
   let temp = 0;
 
   for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+    case 'I':
+      if (arr[i+1] == 'V' || arr[i+1] == 'X')
+        temp -= 1;
+      else
+        temp += 1;
+      break;
+
+    case 'V':
+        temp += 5;
+      break;
     
+    case 'X':
+      if (arr[i+1] == 'L' || arr[i+1] == 'C')
+        temp -= 10;
+      else
+        temp += 10;
+      break;
+      
+    case 'L':
+        temp += 50;
+      break;
+    
+    case 'C':
+      if (arr[i+1] == 'D' || arr[i+1] == 'M')
+        temp -= 100;
+      else
+        temp += 100;
+      break;
+    
+    case 'D':
+        temp += 500;
+      break;
+    
+    case 'M':
+        temp += 1000;
+      break;
+
+    default:
+      break;
+    }    
   }
   return temp;
 };
