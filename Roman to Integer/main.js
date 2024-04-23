eb9c66f81a12de8cdbdb19d391aa6bf71b29f5c8
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-23 21:34:53 +0500

    LC Easy - switch & if

---
+++ Roman to Integer/main.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  let arr = s.split("");
+  let temp = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    
+  }
+  return temp;
+};
+
+console.log(romanToInt("III"));//3
+console.log(romanToInt("LVIII"));//58
