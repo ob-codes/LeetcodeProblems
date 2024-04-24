72b1f578fdea33024a05b605429b3907e4734ee5
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-24 22:24:24 +0500

    [Final] LC Medium - second approach

--- Spiral Matrix/main.js
+++ Spiral Matrix/main.js
@@ -3,37 +3,52 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-  let finalRow = matrix.length;
-  let finalCol = matrix[0].length;
+  let rows = matrix.length;
+  let cols = matrix[0].length;
   let result = [];
 
-  let top = 0, left = 0, bottom = finalRow - 1, right = finalCol - 1;
+  let top = 0, left = 0, bottom = rows - 1, right = cols - 1, count = 0;
   
-  while ((top <= bottom) && (left <= right)) {
+  while (count < rows*cols) {
     //left to right
     for (let i = left; i <= right; i++) {
       result.push(matrix[top][i]);
+      count++;
     }
     top++;
+
+    if (count == rows*cols)
+      break;
+
     //top to bottom
     for (let i = top; i <= bottom; i++) {
       result.push(matrix[i][right]);
+      count++;
     }
     right--;
-    if (top <= bottom) {
-      //right to left
-      for (let i = right; i >= left; i--) {
-        result.push(matrix[bottom][i]);
-      }
-      bottom--;
-    } 
-    if (left <= right) {
-      //bottom to top
-      for (let i = bottom; i >= top; i--) {
-        result.push(matrix[i][left]);
-      }
-      left++;
+
+    if (count == rows*cols)
+      break;
+
+    //right to left
+    for (let i = right; i >= left; i--) {
+      result.push(matrix[bottom][i]);
+      count++;
     }
+    bottom--;
+    
+    if (count == rows*cols)
+      break;
+
+    //bottom to top
+    for (let i = bottom; i >= top; i--) {
+      result.push(matrix[i][left]);
+      count++;
+    }
+    left++;
+
+    if (count == rows*cols)
+      break;
   }
   
   return result;
