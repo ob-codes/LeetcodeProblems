20b8f3ccf2bed1bcbe917ba61fcaedf89a82d246
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-25 00:08:18 +0500

    LC Medium - approach-1

--- Remove Duplicates from Sorted Array-II/main.js
+++ Remove Duplicates from Sorted Array-II/main.js
@@ -3,6 +3,31 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+  if (nums.length <= 2) 
+    return nums.length;
+
+  
+  let result = 0, totalReplaced=0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = i+1; j < nums.length; j++) {      
+      if (nums[j] == nums[i] && nums[j] != 10001){
+        count++;
+
+        if (count > 1) {
+          nums[j] = Math.pow(10, 4) + 1;
+          totalReplaced++;
+        }
+      } 
+    }    
+  }
+  if (totalReplaced == 0)
+    return nums.length;
+
+  nums = nums.sort((a, b) => a-b).slice(0, -totalReplaced);
+
+  return nums.length;
 };
 
 console.log(removeDuplicates([1,1,1,2,2,3])); // 5, nums = [1,1,2,2,3,_]
