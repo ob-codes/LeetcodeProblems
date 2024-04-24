3c64a4d24753374286ca843cd4961afb20000f6f
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-24 21:35:56 +0500

    added logic

--- Spiral Matrix/main.js
+++ Spiral Matrix/main.js
@@ -9,6 +9,32 @@ var spiralOrder = function(matrix) {
 
   let top = 0, left = 0, bottom = finalRow - 1, right = finalCol - 1;
   
+  while ((top <= bottom) && (left <= right)) {
+    //left to right
+    for (let i = left; i <= right; i++) {
+      result.push(matrix[top][i]);
+    }
+    top++;
+    //top to bottom
+    for (let i = top; i <= bottom; i++) {
+      result.push(matrix[i][right]);
+    }
+    right--;
+    if (top <= bottom) {
+      //right to left
+      for (let i = right; i >= left; i--) {
+        result.push(matrix[bottom][i]);
+      }
+      bottom--;
+    } 
+    if (left <= right) {
+      //bottom to top
+      for (let i = bottom; i >= top; i--) {
+        result.push(matrix[i][left]);
+      }
+      left++;
+    }
+  }
   
   return result;
 };
