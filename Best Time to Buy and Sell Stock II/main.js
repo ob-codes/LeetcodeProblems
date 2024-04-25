27ae1abad040512b0aadfcae04a0364ad75ea9e4
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-26 00:33:17 +0500

    [Final] LC Medium

--- Best Time to Buy and Sell Stock II/main.js
+++ Best Time to Buy and Sell Stock II/main.js
@@ -4,6 +4,13 @@
  */
 var maxProfit = function(prices) {
   let maxProfit=0;
+
+  for(let i=1; i< prices.length; i++) {
+      const delta = prices[i] - prices[i-1];
+      if(delta > 0)
+          maxProfit += delta; 
+  }
+  
   return maxProfit;
 };
 
