c002fd82234225882abe1809ed8b253e550f034a
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-27 19:53:04 +0500

    [Final] LC Easy - two pointers approach

--- 121. Best Time to Buy and Sell Stock/main.js
+++ 121. Best Time to Buy and Sell Stock/main.js
@@ -4,13 +4,25 @@
  */
 var maxProfit = function(prices) {
   let maxProfit = 0
-  
+  /*
   for (let i=0; i<prices.length-1; i++) {
       for (let j=i; j<prices.length; j++) {
           if(prices[j] - prices[i] > maxProfit) {
               maxProfit = prices[j] - prices[i];
           }
       }
+  }
+  */
+  let frontPtr = 1, backPtr=0;
+  while(frontPtr < prices.length) {
+      const delta= prices[frontPtr]-prices[backPtr];
+     if (delta > maxProfit) {
+        maxProfit = delta;
+     }
+     if (delta < 0) {
+      backPtr=frontPtr;
+     }
+     frontPtr++;
   }
   
   return maxProfit;
